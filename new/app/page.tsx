2b0460b7459d42c31fbf9da5ad0c1398e3523cbc
palontologist <100496915+palontologist@users.noreply.github.com>
2023-12-12 11:00:19 +0000

    yoo

--- new/app/page.tsx
+++ new/app/page.tsx
@@ -114,13 +114,13 @@ export default function Home() {
             <div className="flex justify-center flex-col sm:flex-row items-start gap-4">
               <Link
                 className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="#"
+                href="https://podcasters.spotify.com/pod/show/21-givenchy"
               >
                 Listen Now
               </Link>
               <Link
                 className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                href="#"
+                href="https://www.youtube.com/@frontforumfocus"
               >
                 Subscribe
               </Link>
@@ -130,7 +130,7 @@ export default function Home() {
       
     </main>
     <footer className="flex items-center justify-between px-6 py-4 bg-white dark:bg-gray-800">
-        <p className="text-gray-600 dark:text-gray-300">© FrontForumFocus. All rights reserved.</p>
+        <p className="text-gray-600 dark:text-gray-300">© frontforumfocus Copyright © 2023 All rights reserved.</p>
         <nav className="space-x-4">
           <Link className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50" href="#">
             Privacy Policy
